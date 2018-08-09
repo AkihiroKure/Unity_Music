9e6f0116b0e9e065462970eb820d16c9d4312b2f
AkihiroKure <akihiro04070927@gmail.com>
2018-08-09 20:26:31 +0900

    first commit

---
+++ Assets/Scripts/Stage_Select_Botton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Stage_Select_Botton : MonoBehaviour
+{
+    public string note;
+    public GameObject SelectSceneManager;// getter
+   
+    // Use this for initialization
+    void Start()
+    { 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            SelectSceneManager.GetComponent<StageSceneManager>().setnote(note);
+            SceneManager.LoadScene("PlayScene");
+        }
+    }
+}
---
+++ Assets/StageSceneManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageSceneManager : MonoBehaviour {
+    public static string note;
+    // getter
+    public static string getnote()
+    {
+        return note;
+    }
+    public void setnote(string str)
+    {
+        
+        note = str;
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
